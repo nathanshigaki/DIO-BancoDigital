d941fe7e93433496ae4f70aec9810090e1617e29
nathanshigaki <nathanshigaki@gmail.com>
2025-04-04 16:39:35 -0400

    Criação do banco e suas funções

--- src/org/bancoDigital/model/Banco.java
+++ src/org/bancoDigital/model/Banco.java
@@ -1,26 +1,61 @@
 package org.bancoDigital.model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.bancoDigital.factory.ContaFactory;
 
 public class Banco {
     private Map<Integer, Conta> contasPorID;
-    private Map<String, List<Conta>> contasPorCPF;
     
     public Banco() {
         this.contasPorID = new HashMap<>();
-        this.contasPorCPF = new HashMap<>();
     }
 
     public Conta criarConta(String tipo, Cliente cliente){
         Conta novaConta = ContaFactory.criarConta(tipo, cliente);
         contasPorID.put(novaConta.getidConta(), novaConta);
-        String cpf = cliente.getCpf();
-        contasPorCPF.computeIfAbsent(cpf, k -> new ArrayList<>()).add(novaConta);
         return novaConta;
+    }
+
+    public List<Conta> listarContas(){
+        List<Conta> contasOrdenadas = new ArrayList<>(contasPorID.values());
+        contasOrdenadas.sort(Comparator.comparing(Conta::getidConta));
+        return contasOrdenadas;
+    }
+
+    public Optional<Conta> buscarContaPorID(int id){
+        return Optional.ofNullable(contasPorID.get(id));
+    }
+
+    public List<Conta> buscarContasPorCPF(String cpf){
+        return contasPorID.values().stream()
+        .filter(c -> c.getCliente().getCpf().equalsIgnoreCase(cpf))
+        .collect(Collectors.toList());
+    }
+
+    public List<Conta> buscarContasPorTipo(String tipo){
+        return contasPorID.values().stream()
+                .filter(c -> c.getTipoConta().equalsIgnoreCase(tipo))
+                .collect(Collectors.toList());
+    }
+
+    public void transferir(int idOrigem, int idDestino, double valor){
+        if (valor <= 0) throw new IllegalArgumentException("Valor deve ser positivo");
+        if (idOrigem == idDestino) throw new IllegalArgumentException("Transferência para a mesma conta não permitida");
+
+        Conta origem = Optional.ofNullable(contasPorID.get(idOrigem))
+                               .orElseThrow(() -> new IllegalArgumentException("Conta origem não encontrada: "+ idOrigem));
+        Conta destivo = Optional.ofNullable(contasPorID.get(idDestino))
+                                .orElseThrow(() -> new IllegalArgumentException("Conta destino não encontrada: "+ idDestino));
+
+        if (origem.getSaldo() < valor) throw new IllegalArgumentException("Saldo insuficiente na conta de origem.");
+        origem.sacar(valor);
+        destivo.depositar(valor);
     }
 }
